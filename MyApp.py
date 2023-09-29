43d28eb7222083730cd818d0f19ee68fc07c4235
Dağıstan Karadeniz <dagistankaradeniz@gmail.com>
2023-09-29 14:37:44 +0100

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import pandas_profiling
+import ydata-profiling
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
-from pandas_profiling import ProfileReport
+from ydata-profiling import ProfileReport
 
 
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv")
