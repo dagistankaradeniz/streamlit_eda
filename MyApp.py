d177b44b97041ddfe7061af82b7d159da583c715
Dağıstan Karadeniz <dagistankaradeniz@gmail.com>
2023-09-29 13:53:13 +0100

    fix indent

--- MyApp.py
+++ MyApp.py
@@ -1,10 +1,10 @@
-    import streamlit as st
-    import pandas as pd
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# My first app
+Hello *world!*
+""")
      
-    st.write("""
-    # My first app
-    Hello *world!*
-    """)
-     
-    df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/NBA_2020.csv')
-    st.line_chart(df)
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/NBA_2020.csv')
+st.line_chart(df)
