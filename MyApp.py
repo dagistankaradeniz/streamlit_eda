4587aab17c374a08bfad318d5b9eba448f3f79cc
Dağıstan Karadeniz <dagistankaradeniz@gmail.com>
2023-09-29 14:09:27 +0100

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -7,5 +7,4 @@ Hello *world!*
 """)
      
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stock_price.csv')
-st.line_chart(df)
 st.line_chart(df, x="Date", y="Close")
