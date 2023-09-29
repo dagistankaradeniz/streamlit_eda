df00e3258e5ad5081f2c791a828da6f176a7ac4d
Dağıstan Karadeniz <dagistankaradeniz@gmail.com>
2023-09-29 14:19:21 +0100

    eda changes added

--- MyApp.py
+++ MyApp.py
@@ -1,10 +1,17 @@
-import streamlit as st
 import pandas as pd
+import pandas_profiling
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
 
 st.write("""
-# My first app
-Hello *world!*
+# **EDA** using *Pandas Profiling* and *Streamlit*
+This is just a sample Hello world kind of app to practice on AutoML and publishing easily (near real time)
 """)
      
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stock_price.csv')
-st.line_chart(df, x="Date", y="Close")
+# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stock_price.csv')
+# st.line_chart(df, x="Date", y="Close")
+
+df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
+pr = df.profile_report()
+
+st_profile_report(pr)
